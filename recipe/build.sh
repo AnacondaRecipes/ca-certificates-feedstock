f810859b2ba6718113254ae3a9405b64a42825c8
Ray Donnelly <mingw.android@gmail.com>
2017-04-09 11:35:55 +0100

    Rewrite to allow bootstrapping
    Since this is a dependency of OpenSSL and therefore Python, it shouldn't use
    Python during the build. Instead switch to the Linux From Scratch shell script.
    
    Also, I've not tested that the Linux from Scratch script can be run on either
    macOS or Windows yet.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,14 @@
 # Create the directory to hold the certificates.
 mkdir -p "${PREFIX}/ssl"
 
-# Copy the certificates from certifi.
-cp -f "${SP_DIR}/certifi/cacert.pem" "${PREFIX}/ssl"
+if [[ $(uname) == Darwin ]]; then
+  curl http://anduin.linuxfromscratch.org/BLFS/other/certdata.txt > certdata.txt
+else
+  wget -c http://anduin.linuxfromscratch.org/BLFS/other/certdata.txt
+fi
+mkdir -p /tmp/ca-cerificates-$$
+bash ./make-ca.sh-20170119 --destdir /tmp/ca-cerificates-$$/ --ssldir ssl --localdir /tmp/no_such_dir
+echo "RESULT $?"
+find ${PREFIX} -name "*.pem" -exec rm {} \;
+mv /tmp/ca-cerificates-$$/ssl/ca-bundle.crt ${PREFIX}/ssl/cacert.pem
 ln -fs "${PREFIX}/ssl/cacert.pem" "${PREFIX}/ssl/cert.pem"
