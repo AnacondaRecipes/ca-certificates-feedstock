0e9f0bc34c03f7504cda6749ef33ae5e765f985f
Ray Donnelly <mingw.android@gmail.com>
2017-08-27 17:24:19 +0100

    Work around conda-build bug (WIP)

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,13 @@ else
   wget -c http://anduin.linuxfromscratch.org/BLFS/other/certdata.txt
 fi
 mkdir -p /tmp/ca-cerificates-$$
-bash ./make-ca.sh-20170119 --destdir /tmp/ca-cerificates-$$/ --ssldir ssl --localdir /tmp/no_such_dir
+# conda-build introduced a bug in source filenames where the checksum gets added.
+if [[ -f ./make-ca_c882ab8b5a.sh-20170514 ]]; then
+  MAKE_CA_SH=./make-ca_c882ab8b5a.sh-20170514
+else
+  MAKE_CA_SH=./make-ca.sh-20170514
+fi
+bash ${MAKE_CA_SH} --destdir /tmp/ca-cerificates-$$/ --ssldir ssl --localdir /tmp/no_such_dir
 echo "RESULT $?"
 find ${PREFIX} -name "*.pem" -exec rm {} \;
 mv /tmp/ca-cerificates-$$/ssl/ca-bundle.crt ${PREFIX}/ssl/cacert.pem
