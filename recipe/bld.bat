6346f9f18891e98edd944b06dbd3d0844c550f50
Ray Donnelly <mingw.android@gmail.com>
2017-09-15 03:52:51 -0700

    Use m2-openssl to build this on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,8 @@ FOR /F "delims=" %%i IN ('cygpath.exe -u "%SP_DIR%"') DO set "SP_DIR=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%STDLIB_DIR%"') DO set "STDLIB_DIR=%%i"
 
-bash -lc .\build.sh
+bash build.sh
+
 if errorlevel 1 exit 1
 copy /y "%PREFIX%"\ssl\cacert.pem "%LIBRARY_PREFIX%"\ssl\cacert.pem
 copy /y "%LIBRARY_PREFIX%"\ssl\cacert.pem "%LIBRARY_PREFIX%"\ssl\cert.pem
--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ else
   wget -c http://anduin.linuxfromscratch.org/BLFS/other/certdata.txt
 fi
 mkdir -p /tmp/ca-cerificates-$$
-bash ./make-ca.sh-20170514 --destdir /tmp/ca-cerificates-$$/ --ssldir ssl --localdir /tmp/no_such_dir
+bash ./make-ca.sh-20170514 --openssl /usr/bin/openssl --destdir /tmp/ca-cerificates-$$/ --ssldir ssl --localdir /tmp/no_such_dir
 echo "RESULT $?"
 find ${PREFIX} -name "*.pem" -exec rm {} \;
 mv /tmp/ca-cerificates-$$/ssl/ca-bundle.crt ${PREFIX}/ssl/cacert.pem
