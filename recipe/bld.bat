7e16d122865fb5491fb0199a0e6625d8a9082bed
Jonathan Helmus <jjhelmus@gmail.com>
2018-04-02 13:47:48 -0500

    source from curl.haxx.se
    Use the certificate bundle provided by the curl authors as the source for this
    package.  Updates and sha256 are published to:
    https://curl.haxx.se/docs/caextract.html

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,5 @@
-copy %RECIPE_DIR%\build.sh .
-
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PYTHON%"') DO set "PYTHON=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%RECIPE_DIR%"') DO set "RECIPE_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%SP_DIR%"') DO set "SP_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%STDLIB_DIR%"') DO set "STDLIB_DIR=%%i"
-
-bash build.sh
-
+xcopy /y "%SRC_DIR%"\cacert.pem "%LIBRARY_PREFIX%"\ssl\
 if errorlevel 1 exit 1
-copy /y "%PREFIX%"\ssl\cacert.pem "%LIBRARY_PREFIX%"\ssl\cacert.pem
 copy /y "%LIBRARY_PREFIX%"\ssl\cacert.pem "%LIBRARY_PREFIX%"\ssl\cert.pem
-del /y "%PREFIX%"\ssl\cert.pem
-del /y "%PREFIX%"\ssl\cacert.pem
+if errorlevel 1 exit 1
 exit 0
